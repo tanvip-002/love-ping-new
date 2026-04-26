948227878675f396404ff912178ee0cc2b4316a2
Tanvi Pardeshi <tanvipardeshi002@gmail.com>
2026-04-26 16:51:27 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import pingRoutes from "./routes/pingRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -9,14 +10,27 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
+}
+
 app.use(express.json());
 
-
 app.use("/api/ping", pingRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
